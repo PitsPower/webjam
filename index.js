07614b8bc84f69c1927c0a01c9161fc783baafd4
Wheatley <josepholiver247@msn.com>
2017-09-24 18:53:03 +0000

    Removed travis-ci and GitHub stuff (it was annoying)

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var Discord = require('discord.js');
 var client = new Discord.Client();
 
-console.log(JSON.stringify(process, null, 2));
-client.login(process.env.DISCORD_KEY);
+client.login('MzYxNDk5ODY0NjM2MzI1ODg5.DKl1rg.5Yt0_FVA21wFnFO36D8f4yeVrgI');
 
 client.on('ready', function() {
 	client.channels.find('id', '361305953620197386').sendMessage("Hello! This is a test.");
