2890049cfb1435e8782b8421b956aebe7b587bcc
Wheatley <josepholiver247@msn.com>
2017-09-24 18:04:14 +0000

    Testing env variables

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var Discord = require('discord.js');
 var client = new Discord.Client();
 
-console.log(global);
-client.login(global.DISCORD_KEY);
+console.log(process.env);
+client.login(process.env.DISCORD_KEY);
 
 client.on('ready', function() {
 	client.channels.find('id', '361305953620197386').sendMessage("Hello! This is a test.");
