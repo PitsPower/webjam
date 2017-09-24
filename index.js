d30977d72a1c106211640959b1d1683b362cd1c7
Wheatley <josepholiver247@msn.com>
2017-09-24 18:58:56 +0000

    Hid DISCORD_KEY

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var Discord = require('discord.js');
 var client = new Discord.Client();
 
-client.login('MzYxNDk5ODY0NjM2MzI1ODg5.DKl1rg.5Yt0_FVA21wFnFO36D8f4yeVrgI');
+client.login(process.env.DISCORD_KEY);
 
 client.on('ready', function() {
-	client.channels.find('id', '361305953620197386').sendMessage("Hello! This is a test.");
+	client.channels.find('id', '361305953620197386').sendMessage("This is yet another test. Can you believe it?");
 });
