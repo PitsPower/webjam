081919b50c6fa31d11ec714a77a5347d49653d76
Wheatley <josepholiver247@msn.com>
2017-09-24 17:47:22 +0000

    Testing global vars

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Discord = require('discord.js');
 var client = new Discord.Client();
 
 console.log(process.env);
-client.login(process.env.DISCORD_KEY);
+client.login(DISCORD_KEY);
 
 client.on('ready', function() {
 	client.channels.find('id', '361305953620197386').sendMessage("Hello! This is a test.");
