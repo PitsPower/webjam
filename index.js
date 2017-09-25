5e583486a7a0b118bbbd4ffffb81f2e71db63bee
WheatleyTheCoder <josepholiver247@msn.com>
2017-09-25 17:47:46 +0000

    Added simple command detection

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var client = new Discord.Client();
 
 client.login(process.env.DISCORD_KEY);
 
+var prefix = 'j!';
+client.on('message', function(msg) {
+    if (msg.content.indexOf(prefix)==0) {
+        console.log(msg.content);
+    }
+});
 client.on('ready', function() {
-    
+    console.log('Ready and waiting, captain!');
 });
