0c11e80a7cdf149748d4c47d6aa34c762216f357
WheatleyTheCoder <josepholiver247@msn.com>
2017-09-26 19:25:39 +0000

    Added ability to turn favicons into emojis

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
 var Discord = require('discord.js');
 var client = new Discord.Client();
 
+var request = require('request');
+var favicon = require('favicon');
+
 client.login(process.env.DISCORD_KEY);
 
 var prefix = 'j!';
 client.on('message', function(msg) {
     if (msg.content.indexOf(prefix)==0) {
-        console.log(msg.content);
+        var commandArguments = msg.content.replace(prefix, '').split(' ');
+        var commandName = commandArguments.shift();
     }
 });
 client.on('ready', function() {
     console.log('Ready and waiting, captain!');
 });
+
+function faviconEmoji(siteName, cb) {
+    var siteUrl = 'https://'+siteName+'.neocities.org/';
+    favicon(siteUrl, function(err, data) {
+        if (err) return console.log(err);
+        if (data) {
+            console.log(data);
+            if (data.indexOf('data:image')>-1) {
+                data = new Buffer(data.replace(/^data:image\/(png|gif|jpeg);base64,/, ''), 'base64');
+            } else if (data.indexOf(siteUrl)==-1 && data.indexOf('https')!=0) {
+                data = siteUrl + data;
+            }
+            
+            var emojiName = 'favicon_'+siteName;
+            var emojiGuild = client.guilds.find('id', '308369594291191819');
+            
+            emojiGuild.createEmoji(data, emojiName).then(function(emoji) {
+                cb({
+                    text: '<:'+emojiName+':'+emoji.id+'>',
+                    delete: function() {
+                        emojiGuild.deleteEmoji(emoji)
+                    }
+                });
+            });
+        } else {
+            cb(null);
+        }
+    });
+}
